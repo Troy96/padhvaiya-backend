204bdc85b3345041bad879f4da22ea16f6eec6b5
Tuhin Roy <troy0870@gmail.com>
2020-02-09 22:28:50 +0530

    binded socket on same port

--- chat-server/index.js
+++ chat-server/index.js
@@ -1,57 +1,46 @@
-const app = require('express')();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server,{
-    pingInterval: 600000,
-    pingTimeout: 600000
-});
+
 const {Group} = require('../models/group');
 const {User} = require('../models/user');
 const { ChatController } = require('../controllers/ChatController');
-require('./../db/conn');
 
 const chatController = new ChatController();
 
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
+module.exports = function(io){
+    io.on('connection', client => {
 
-io.on('connection', client => {
+        console.log('connected', client.id)
+    
+        client.on('userIsOnline', chatController.handlerUserIsOnline);
+    
+        client.on('userIsOffline', chatController.handlerUserIsOffline);
+    
+        client.on('getGroupMembers', async groupId => {
+            const groupObj = await Group.findById({ _id: groupId })
+                .populate('admins')
+                .populate('members');
+            const memberList = groupObj['admins'].concat(groupObj['members']);
+            client.emit('onGroupMembers', memberList);
+        })
+    
+        client.on('join', async (joinParams, callback)=>{
+            const userObj = await User.findById({_id: joinParams.userId});
+            client.join(joinParams.groupId);
+            client.emit('chat message', `Welcome to the group, ${userObj.first_name}`);
+            client.broadcast.to(joinParams.groupId).emit('chat message', `${userObj.first_name} joined`)
+            callback();
+        })
+    
+    
+        client.on('disconnect', ()=> {
+            console.log(client.id, 'disconnected')
+        });
+    
+        client.on('chat message', (obj, callback) => {
+           io.to(obj.to).emit('chat message', chatController.createMessage(obj));
+           callback();
+        });
+    });    
+}
 
-    console.log('connected', client.id)
 
-    client.on('userIsOnline', chatController.handlerUserIsOnline);
-
-    client.on('userIsOffline', chatController.handlerUserIsOffline);
-
-    client.on('getGroupMembers', async groupId => {
-        const groupObj = await Group.findById({ _id: groupId })
-            .populate('admins')
-            .populate('members');
-        const memberList = groupObj['admins'].concat(groupObj['members']);
-        client.emit('onGroupMembers', memberList);
-    })
-
-    client.on('join', async (joinParams, callback)=>{
-        const userObj = await User.findById({_id: joinParams.userId});
-        client.join(joinParams.groupId);
-        client.emit('chat message', `Welcome to the group, ${userObj.first_name}`);
-        client.broadcast.to(joinParams.groupId).emit('chat message', `${userObj.first_name} joined`)
-        callback();
-    })
-
-
-    client.on('disconnect', ()=> {
-        console.log(client.id, 'disconnected')
-    });
-
-    client.on('chat message', (obj, callback) => {
-       io.to(obj.to).emit('chat message', chatController.createMessage(obj));
-       callback();
-    });
-});
-
-server.listen(3001, err => {
-    if (err) throw err;
-    console.log('Chat server listeninig...');
-})
