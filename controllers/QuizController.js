3716188b46ad68553304de4c8ce14bf4e19bae89
Tuhin Roy <troy0870@gmail.com>
2020-06-06 17:51:30 +0530

    add quiz controller methods

---
+++ controllers/QuizController.js
@@ -0,0 +1,31 @@
+
+
+
+class QuizController {
+    constructor() { }
+
+    async getQuizById() {
+
+    }
+
+    async checkIfQuizIsOpen() {
+
+    }
+
+    async registerParticipant() {
+
+    }
+
+    async disqualifyParticipant() {
+
+    }
+
+    async getParticipants() {
+
+    }
+
+}
+
+module.exports = {
+    QuizController
+}
