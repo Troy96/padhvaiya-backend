749b4b17d6f8072319619a0fc5c2493eab544de6
Tuhin Roy <troy0870@gmail.com>
2020-06-06 17:30:57 +0530

    add quiz-participant model

---
+++ models/quiz-participant.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const QuizParticipantSchema = new mongoose.Schema({
+    quizId: { type: mongoose.Types.ObjectId, ref: 'Quiz' },
+    name: { type: String, default: null },
+    email: { type: String, unique: true, validate: { validator: validator.isEmail, message: 'Not a valid Email' } },
+    phone: { type: Number, default: null, unique: true },
+    age: { type: Number, default: null },
+    isRegistered: { type: Boolean, default: true },
+    isQualified: { type: Boolean, default: true }
+});
+
+const QuizParticipantModel = mongoose.model('QuizParticipant', QuizParticipantSchema, 'QuizParticipant');
+
+module.exports = {
+    QuizParticipantModel
+}
