b58ec858d8f055eb8a5f6add66b09f746b0d8fe5
Tuhin Roy <troy0870@gmail.com>
2020-06-06 17:23:55 +0530

    add quiz-modal

---
+++ models/quiz.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const QuizSchema = new mongoose.Schema({
+    name: { type: String, default: null },
+    desc: { type: String, default: null },
+    isLive: { type: Boolean, default: false },
+    isOpenForRegistration: { type: Boolean, default: false },
+    timestamp: { type: Number, default: null },
+    createdAt: { type: Date, default: Date.now() },
+    updatedAt: { type: Date, default: Date.now() }
+});
+
+const QuizModal = mongoose.model('Quiz', QuizSchema, 'Quiz');
+
+module.exports = {
+    QuizModal
+}
