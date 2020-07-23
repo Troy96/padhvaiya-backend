3cb79b7d1548e0e2d2207926b9365feb480049d7
Tuhin Roy <troy0870@gmail.com>
2020-07-23 10:30:47 +0530

    fix

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ const app = express();
 app.use(fileUpload());
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 //
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
